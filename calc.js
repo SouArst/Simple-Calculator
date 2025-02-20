b264507add6fa72905a19c132ee1accb64c0229c
SouArst <alexsandrorodriguestdb@gmail.com>
2025-02-20 17:51:09 -0300

    Add Functions = Theme Switcher - Copy To Clipboard - Error Message

--- calc.js
+++ calc.js
@@ -34,8 +34,39 @@
  document.getElementById('equal').addEventListener('click', calculate)
 
  function calculate () {
+  resultInput.value = 'Error!'
+  resultInput.classList.add('error')
   const result = eval (input.value)
   resultInput.value = result
+  resultInput.classList.remove('error')
  }
+
+ document.getElementById('copyToClipboard').addEventListener('click',function (ev){
+  const buttonCopy = ev.currentTarget
+  if (buttonCopy.innerText === 'Copy'){
+    buttonCopy.innerText = 'Copied!'
+    buttonCopy.classList.add('success')
+    navigator.clipboard.writeText(resultInput.value)
+  } else {
+    buttonCopy.innerText ='Copy'
+    buttonCopy.classList.remove ('success')
+  }
+ })
+
+ document.getElementById ('themeSwitcher').addEventListener('click', function (){
+  if (main.dataset.theme === 'dark') {
+    root.style.setProperty ('--bg-color','#f1f5f9')
+    root.style.setProperty ('--border-color','#aaa')
+    root.style.setProperty ('--font-color','#212529')
+    root.style.setProperty ('--primary-color','#26834a')
+    main.dataset.theme = 'light'
+  } else {
+    root.style.setProperty ('--bg-color','#212529')
+    root.style.setProperty ('--border-color','#666')
+    root.style.setProperty ('--font-color','#f1f5f9')
+    root.style.setProperty ('--primary-color','#4dff91')
+    main.dataset.theme = 'dark'
+  }
+ })
 
  
